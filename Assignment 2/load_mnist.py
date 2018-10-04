7c3a5ab99623f89db4ae7f434a2ea8042af97337
Rishabh Patil <rishabh@particle41.com>
2018-10-04 13:14:13 -0700

    Modified load_mnist

--- Assignment 2/load_mnist.py
+++ Assignment 2/load_mnist.py
@@ -3,8 +3,7 @@ import os
 import pdb
 import matplotlib.pyplot as plt
 
-datasets_dir = '../data/'
-
+datasets_dir = 'data/'
 
 def one_hot(x, n):
     if type(x) == list:
@@ -20,21 +19,18 @@ def mnist(noTrSamples=1000, noTsSamples=100, digit_range=[0, 10], noTrPerClass=1
     fd = open(os.path.join(data_dir, 'train-images-idx3-ubyte'))
     loaded = np.fromfile(file=fd, dtype=np.uint8)
     trData = loaded[16:].reshape((60000, 28*28)).astype(float)
-
     fd = open(os.path.join(data_dir, 'train-labels-idx1-ubyte'))
     loaded = np.fromfile(file=fd, dtype=np.uint8)
     trLabels = loaded[8:].reshape((60000)).astype(float)
-
     fd = open(os.path.join(data_dir, 't10k-images-idx3-ubyte'))
     loaded = np.fromfile(file=fd, dtype=np.uint8)
     tsData = loaded[16:].reshape((10000, 28*28)).astype(float)
-
     fd = open(os.path.join(data_dir, 't10k-labels-idx1-ubyte'))
     loaded = np.fromfile(file=fd, dtype=np.uint8)
     tsLabels = loaded[8:].reshape((10000)).astype(float)
 
-    trData = trData/255.
-    tsData = tsData/255.
+    trData = trData/255
+    tsData = tsData/255
 
     tsX = np.zeros((noTsSamples, 28*28))
     trX = np.zeros((noTrSamples, 28*28))
@@ -47,23 +43,20 @@ def mnist(noTrSamples=1000, noTsSamples=100, digit_range=[0, 10], noTrPerClass=1
         idl = np.where(trLabels == ll)
         idl = idl[0][: noTrPerClass]
         idx = list(range(count*noTrPerClass, (count+1)*noTrPerClass))
-        trX[idx, :] = trData[idl[0], :]
-        trY[idx] = trLabels[idl[0]]
+        trX[idx, :] = trData[idl, :]
+        trY[idx] = trLabels[idl]
         # Test data
         idl = np.where(tsLabels == ll)
         idl = idl[0][: noTsPerClass]
         idx = list(range(count*noTsPerClass, (count+1)*noTsPerClass))
-        tsX[idx, :] = tsData[idl[0], :]
-        tsY[idx] = tsLabels[idl[0]]
+        tsX[idx, :] = tsData[idl, :]
+        tsY[idx] = tsLabels[idl]
         count += 1
     
     np.random.seed(1)
     test_idx = np.random.permutation(tsX.shape[0])
     tsX = tsX[test_idx,:]
     tsY = tsY[test_idx]
-
-    trX = np.squeeze(trX).T
-    tsX = np.squeeze(tsX).T
     trY = trY.reshape(1, -1)
     tsY = tsY.reshape(1, -1)
     return trX, trY, tsX, tsY
